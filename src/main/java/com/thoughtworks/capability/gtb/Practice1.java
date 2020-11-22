c5a0ca68ee4625962377f67cfa2b388c8c9c7f54
yanyuxin <617702281@qq.com>
2020-11-22 13:02:21 +0800

    [yyx] finish practice 1

--- src/main/java/com/thoughtworks/capability/gtb/Practice1.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice1.java
@@ -11,6 +11,14 @@ import java.time.LocalDate;
 public class Practice1 {
 
   public static long getDaysBetweenNextLaborDay(LocalDate date) {
-    return 0;
+    int month = date.getMonthValue();
+    int year = date.getYear();
+    if (month <= 5) {
+      LocalDate  mayDay = LocalDate.of(year, 5, 1);
+      return mayDay.toEpochDay() - date.toEpochDay();
+    }else{
+      LocalDate mayDay = LocalDate.of(year + 1, 5, 1);
+      return mayDay.toEpochDay() - date.toEpochDay();
+    }
   }
 }
