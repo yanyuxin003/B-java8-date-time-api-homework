e2d713953f4b1e9e229255b1c61699101e8eeda6
yanyuxin <617702281@qq.com>
2020-11-22 13:18:09 +0800

    [yyx] finish practice 2

--- src/main/java/com/thoughtworks/capability/gtb/Practice2.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice2.java
@@ -2,6 +2,9 @@ package com.thoughtworks.capability.gtb;
 
 import java.time.LocalDate;
 
+import static java.util.Calendar.FRIDAY;
+import static java.util.Calendar.SATURDAY;
+
 /**
  * 对任意日期获取下一个工作日, 不考虑节假日
  *
@@ -11,6 +14,12 @@ import java.time.LocalDate;
 public class Practice2 {
 
   public static LocalDate getNextWorkDate(LocalDate date) {
-    return null;
+    int dayOfWeek = date.getDayOfWeek().getValue();
+    if (dayOfWeek==5) {
+      return date.plusDays(3);
+    } else if (dayOfWeek==6) {
+      return date.plusDays(2);
+    }
+      return date.plusDays(1);
   }
 }
